b76b7522c9cc967d07f2a51fff477ed3dc54faca
Wahyu Arif Purnomo <warifp@gmail.com>
2019-04-04 02:39:05 +0700

    Initial commit

---
+++ run.php
@@ -0,0 +1,40 @@
+<?php
+
+echo "List \t\t: ";
+$list = trim(fgets(STDIN));
+
+echo "Refferal \t\t: ";
+$reff = trim(fgets(STDIN));
+
+$file = file_get_contents("$list");
+$data = explode("\n",$file);
+
+for($a=1;$a<count($data);$a++){
+    $data1 = explode("|",$data[$a]);
+    $email = $data1[0];
+    $post = "email=$email&password=%40Aq123456&referral_id=$reff&monetize=1";
+    $arr = array("\r"," ");
+    $headers = array();
+    $headers[] = "content-type: application/x-www-form-urlencoded";
+    $headers[] = "Accept-Encoding: gzip";
+    $headers[] = "Host: api.bigtoken.com";
+    $headers[] = "user-agent: Redmi 5A_7.1.2_1.0.6";
+    $headers[] = "accept: application/json";
+    $url = "https://api.bigtoken.com/signup";
+    $h = explode("\n",str_replace($headers,"",""));
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    $obj = json_encode($result);
+    echo "\e[1;92m$a. Done Register Email => $email\e[0m\n";
+    print_r($obj);
+    echo "\n";
+    curl_close($ch); 
+}
+?>
